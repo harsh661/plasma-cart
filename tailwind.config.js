8dec91376f9a32450e9555b2da39b189e3bd47b1
harsh661 <plasmaharsh@gmail.com>
2023-07-28 19:34:56 +0530

    chore: installed initial packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        'accent': '#FF660E',
+        'black': '#202020',
+        'gray': '#999',
+        'light-gray': '#E3E3E3',
+      }
     },
   },
   plugins: [],
