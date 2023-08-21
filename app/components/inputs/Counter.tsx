6df56ca1cc001bd4262a55341daa3e1b659ac351
harsh661 <plasmaharsh@gmail.com>
2023-08-21 12:48:50 +0530

    minor bug fix

--- app/components/inputs/Counter.tsx
+++ app/components/inputs/Counter.tsx
@@ -3,13 +3,24 @@ import {IoMdAdd, IoMdRemove} from "react-icons/io"
 
 const Counter = () => {
   const [count, setCount] = useState<number>(1)
+
+  const handleChange = (change: string) => {
+    if(change === 'increment') {
+      setCount(prev => prev + 1)
+    }
+    if(change === 'decrement') {
+      if(count <= 1) return
+      setCount(prev => prev - 1)
+    }
+  } 
+
   return (
     <div className="flex items-center bg-light-gray/50 justify-between w-max rounded-full overflow-hidden">
-      <div className="py-2 px-4 cursor-pointer" onClick={() => setCount((prev) => prev - 1)}>
+      <div className="py-2 px-4 cursor-pointer" onClick={() => handleChange('decrement')}>
         <IoMdRemove />
       </div>
       <div className="p-2 w-10 text-center">{count}</div>
-      <div className="py-2 px-4 cursor-pointer" onClick={() => setCount((prev) => prev + 1)}>
+      <div className="py-2 px-4 cursor-pointer" onClick={() => handleChange('increment')}>
         <IoMdAdd />
       </div>
     </div>
